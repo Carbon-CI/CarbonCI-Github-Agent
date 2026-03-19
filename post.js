557823fa5f348f1e04399d896ca4ca0a4091ba7a
Galopin098 <valentingascoin@outlook.fr>
2026-03-19 13:59:36 +0100

    Update post.js

--- post.js
+++ post.js
@@ -1,15 +1,18 @@
 const { execSync } = require('child_process');
 const { env } = require('process');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
-// Read values saved by pre.js via GitHub Actions state (STATE_ prefix)
-const apiKey   = env['STATE_CARBON_CI_API_KEY'];
-const project  = env['STATE_CARBON_CI_PROJECT'];
-const pipeline = env['STATE_CARBON_CI_PIPELINE'];
-const job      = env['STATE_CARBON_CI_JOB'];
-const runner   = env['STATE_CARBON_CI_RUNNER'];
+// Read values written by pre.js
+const stateFile = path.join(os.tmpdir(), `carbon-ci-${env['GITHUB_RUN_ID']}-${env['GITHUB_JOB']}.json`);
+const { apiKey, project, pipeline, job, runner } = JSON.parse(fs.readFileSync(stateFile, 'utf8'));
 
 console.log('Stopping Carbon CI monitoring...');
 execSync(
   `carbon-ci stop --api-key "${apiKey}" --project "${project}" --pipeline "${pipeline}" --job "${job}" --runner "${runner}"`,
   { stdio: 'inherit' }
 );
+
+// Cleanup
+fs.unlinkSync(stateFile);
