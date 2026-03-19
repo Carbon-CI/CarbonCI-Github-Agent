44a75caccfa7f2a924fb57aa3317e3141335090d
Galopin098 <valentingascoin@outlook.fr>
2026-03-19 14:08:02 +0100

    Update Carbon CI stop command to use env variable
    Refactor stopping Carbon CI to use CARBON_CI_API_KEY from env.

--- post.js
+++ post.js
@@ -4,15 +4,17 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 
-// Read values written by pre.js
+// Read api-key written by pre.js
 const stateFile = path.join(os.tmpdir(), `carbon-ci-${env['GITHUB_RUN_ID']}-${env['GITHUB_JOB']}.json`);
-const { apiKey, project, pipeline, job, runner } = JSON.parse(fs.readFileSync(stateFile, 'utf8'));
+const { apiKey } = JSON.parse(fs.readFileSync(stateFile, 'utf8'));
 
 console.log('Stopping Carbon CI monitoring...');
-execSync(
-  `carbon-ci stop --api-key "${apiKey}" --project "${project}" --pipeline "${pipeline}" --job "${job}" --runner "${runner}"`,
-  { stdio: 'inherit' }
-);
+
+// install.sh stop reads CARBON_CI_API_KEY env var
+execSync('carbon-ci stop', {
+  stdio: 'inherit',
+  env: { ...env, CARBON_CI_API_KEY: apiKey }
+});
 
 // Cleanup
 fs.unlinkSync(stateFile);
